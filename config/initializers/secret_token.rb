bd644b78cd0ab35504924f8c68e730019b5deab8
Bruno Dias <brunoald@gmail.com>
2012-07-06 01:26:53 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAppBootstrapHeroku::Application.config.secret_token = 'a3de0ce9a384af2899e85c64ee113b36e0d121e95e1674bdb424217a53abe50ab8856d2188a23ecc5d5870a18a3bb3c8e930d45ada9968f21eeb9ee229f107cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAppBootstrapHeroku::Application.config.session_store :cookie_store, key: '_rails-app-bootstrap-heroku_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsAppBootstrapHeroku::Application.config.session_store :active_record_store
